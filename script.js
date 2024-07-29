be76196763daf7757e102fb853053a098dbe5e50
sillebo <92082373+sillebo@users.noreply.github.com>
2024-07-29 16:08:24 +0200

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Setting up the canvas
 var width = 1000;
 var height = 1000;
 var margin = 30;
@@ -9,34 +10,55 @@ var canvas = d3
   .attr("height", height)
   .style("background-color", "lightgrey");
 
+  // Loading and processing data
 d3.json("tempature.json").then(function (data) {
-  console.log("Entire Data Object:", data);
+  console.log("Entire Data Object:", data); 
 
   // Extract the relevant data
   var waterData = [];
   var yearData = [];
   Object.keys(data).forEach(function (key) {
     var value = data[key];
-    yearData = d3.map(data, (d) => d.name);
+    yearData = d3.map(data, (d) => d.name); // yearData extracts the names (years) from the data object
 
     if (Array.isArray(value.data)) {
-      waterData = value.data.filter((d) => typeof d == "number");
+      waterData = value.data.filter((d) => typeof d == "number"); // yearData extracts the data (water) from the data object and removes non-numeric entries
     }
   });
 
-  console.log("Filtered Waterdata Array:", waterData);
+  console.log("Filtered Waterdata Array:", waterData); // The console.log function is used to output information to the web console.
   console.log("years", yearData);
   // Get the last data point
-  var lastDataPoint = waterData[waterData.length - 1];
-  console.log("Last Data Point:", lastDataPoint);
+  var lastDataPoint = waterData[waterData.length - 1]; // Retries the last element in the waterData array
+  console.log("Last Data Point:", lastDataPoint); // Logs the last data point
 
+
+
+  Function draw()
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
   // Sort array lowest -> highest
   //waterData.sort(d3.ascending);
 
   // Compute min and max for scaling
-  var minWater = d3.min(waterData);
-  var maxWater = d3.max(waterData);
-  var waterScale = d3
+  var minWater = d3.min(waterData); // Calculates min value in the waterData array
+  var maxWater = d3.max(waterData); // Calculates max value in the waterData array 
+  var waterScale = d3 // Creates a linear scale.
     .scaleLinear()
     .domain([0, waterData.length - 1])
     .range([margin, width - margin]);
@@ -62,3 +84,4 @@ function draw(waterData, waterScale, radScale) {
     .attr("fill", "blue")
     .attr("stroke", "white");
 }
+*/
